bb8b4cc246a9456659fb8542baec5c5720b63c8b
Luiz <luizcsnvartuli@gmail.com>
2021-09-08 15:49:55 -0300

    movimentacao em progresso

--- matriz_teste.s
+++ matriz_teste.s
@@ -1,9 +1,12 @@
 .data
 
-LINHA1:	.word 0,0,0
-LINHA2:	.word 1,0,0
-LINHA3:	.word 0,0,0
-MATRIZ:	.word LINHA1,LINHA2,LINHA3
+LINHA0:	.word 0,0,2,0
+LINHA1:	.word 2,0,0,1
+LINHA2:	.word 0,0,0,2
+LINHA3:	.word 0,2,0,0
+MATRIZ:	.word LINHA0,LINHA1,LINHA2,LINHA3
+PLAYER_POS:	.byte 3,1	#coluna,linha
+M_SIZE:	.word 4,4		#n_linhas, n_colunas
 
 esp:	.string " "
 n:		.string "\n"
@@ -12,27 +15,140 @@ n:		.string "\n"
 
 MAIN:
 
+
 	la a0,MATRIZ
-	li a1,3
-	li a2,3
+	la t0,M_SIZE
+	lw a1,0(t0)
+	lw a2,4(t0)
 	jal M_SHOW
 
+	jal GET_KEY
+	
+	bltz a0,END
+	
+	la a0,n
+	li a7,4
+	ecall
+	
+	j MAIN
+	
+	#la a0,MATRIZ
+	#li a1,3
+	#li a2,3
+	#jal M_SHOW
 
-
+END:
 	li a7,10
 	ecall
 
-#
+# a = 0x61
+# d = 0x64
+# e = 0x65
+# q = 0x71
+# s = 0x73
+# w = 0x77
 #
 GET_KEY:
+	addi sp,sp,-4
+	sw ra,0(sp)
+	
+	
 	li a7,12
 	ecall
 
+	li t0,0x61
+	beq a0,t0,a
+	
+	li t0,0x64
+	beq a0,t0,d
+	
+	li t0,0x65
+	beq a0,t0,e
+
+	li t0,0x73
+	beq a0,t0,s
+	
+	li t0,0x77
+	beq a0,t0,w
+
+	j GET_KEY_END
+
+a:
+
+	li a0,-1
+	li a1,1
+	la a2,PLAYER_POS
+	la a3,MATRIZ
+	la a4,M_SIZE
+
+	jal MOVE_H
+
+	#lb t0,0(s1)		#coluna onde o player esta
+	#beqz t0,a_END	#se for a coluna 0, n move para esquerda
+	#lb t1,1(s1)		#linha onde o player esta
+	#slli t1,t1,2
+	#add t1,t1,s0	#endereco da linha na matriz
+	#lw t2,0(t1)		#t2 = linha
+	#addi t0,t0,-1
+	#slli t5,t0,2
+	#add t5,t5,t2
+	#lw t3,0(t5)		#t3 = o que tem a esquerda do player
+	#li t4,2			#objeto que impede o player
+	#beq t3,t4,a_END
+	#li t4,1
+	#sw t4,0(t5)
+	#sw zero,4(t5)
+	#sb t0,0(s1)
+	
+#a_END:
+	li a0,0
+	j GET_KEY_END
+
+d:
+
+	li a0,1
+	li a1,1
+	la a2,PLAYER_POS
+	la a3,MATRIZ
+	la a4,M_SIZE
+
+	jal MOVE_H
+
+
+	li a0,0
+	j GET_KEY_END
+
+
+e:
+	li a0,-1
+	j GET_KEY_END
+
+s:
 
 
 
 
-#a0= int vetor[], a1= len(vetor)
+	li a0,0
+	j GET_KEY_END
+
+w:
+
+
+
+
+
+	li a0,0
+	j GET_KEY_END
+
+GET_KEY_END:
+	lw ra,0(sp)
+	addi sp,sp,4
+	ret
+
+
+
+
+#a0= int vetor[], a1= int len(vetor)
 #return void
 V_SHOW:
 	mv t0,a0
@@ -90,3 +206,4 @@ M_SHOW_LOOP:
 	
 	ret
 	
+.include "teste.s"
---
+++ teste.s
@@ -0,0 +1,71 @@
+.text
+
+#
+#a0= unidades a mover (negativo para esquerda, positivo para direita)
+#a1= o que vai ser movido (1 para player, etc)
+#a2= posicao do que vai ser movido (coluna, linha)
+#a3= matriz do jogo
+#a4= tamanho da matriz do jogo (n_linhas, n_colunas)
+MOVE_H:
+	lb t0,0(a2)				#t0 = indice da coluna onde o objeto esta
+	add t1,t0,a0			#t1 = indice da coluna destino
+	bltz t1,MOVE_H_END		#se o objeto n conseguir mover tanto para esquerda, n move
+	lw t5,4(a4)				#t5 = numero de colunas
+	bge t1,t5,MOVE_H_END	#se o objeto n conseguir mover tanto para a direita, n move
+	lb t2,1(a2)				#t2 = indice da linha onde o objeto esta
+	slli t2,t2,2			#como cada linha e um word, multiplica o indice por 4
+	add t2,t2,a3			#t2 = endereco da linha na matriz
+	lw t3,0(t2)				#t3 = linha
+	slli t2,t1,2
+	add t2,t2,t3			#t2 = endereco destino
+	lw t4,0(t2)				#t4 = objeto no endereco destino
+	li t5,2
+	beq t4,t5,MOVE_H_END	#se o objeto no destino for parede, n move
+	slli t4,t0,2
+	add t4,t4,t3			#t4 = endereco original
+	sw zero,0(t4)			#coloca 0 no endereco original
+	sw a1,0(t2)				#coloca o objeto no novo endereco
+	sb t1,0(a2)				#muda a posicao do objeto
+
+MOVE_H_END:
+	ret
+
+
+
+#
+#a0= unidades a mover (negativo para cima, positivo para baixo)
+#a1= o que vai ser movido (1 para player, etc)
+#a2= posicao do que vai ser movido (coluna, linha)
+#a3= matriz do jogo
+#a4= tamanho da matriz do jogo (n_linhas, n_colunas)
+MOVE_V:
+	addi sp,sp,-4
+	sw s0,0(sp)
+
+	lb t0,1(a2)				#t0 = indice da linha onde o objeto esta
+	add t1,t0,a0			#t1 = indice da linha destino
+	bltz t1,MOVE_V_END		#se o objeto n conseguir mover tanto para cima, n move
+	lw t5,0(a4)				#t5 = numero de linhas
+	bge t1,t5,MOVE_V_END	#se o objeto n conseguir mover tanto para a baixo, n move
+	lb t2,0(a2)				#t2 = indice da coluna onde o objeto esta
+	slli t2,t2,2			#t2 = offset da coluna
+	slli t3,t0,2
+	add t3,t3,a3			#t3 = endereco da linha original
+	lw t3,0(t3)				#t3 = linha original
+	slli t4,t1,2
+	add t4,t4,a3			#t4 = endereco da linha destino
+	lw t4,0(t4)				#t4 = linha destino
+	add t5,t2,t4			#t5 = endereco destino
+	lw t6,0(t5)				#t6 = objeto no endereco destino
+	li s0,2
+	beq t6,s0,MOVE_V_END	#se o objeto no destino for parede, n move
+	add t6,t2,t3			#t6 = endereco original
+	lw zero,0(t6)			#coloca 0 no endereco original
+	lw a1,0(t5)				#coloca o objeto no novo endereco
+	lb t1,1(a2)				#muda a posicao do objeto
+
+
+MOVE_V_END:
+	lw s0,0(sp)
+	addi sp,sp,4
+	ret
